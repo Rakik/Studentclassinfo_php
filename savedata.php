962a3b3c57869c8f860947927f46aca7baa68da7
Rakik <mostakalrakik63@gmail.com>
2021-07-13 02:59:46 +0600

    Hi

---
+++ savedata.php
@@ -0,0 +1,17 @@
+<?php
+
+echo $stu_name = $_POST['sname'];
+echo $stu_address = $_POST['saddress'];
+echo $stu_class = $_POST['class'];
+echo $stu_phone = $_POST['sphone'];
+$conn = mysqli_connect("localhost","root","","crud") or die("Connection Invalid");
+   
+$sql ="INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
+
+$result = mysqli_query($conn,$sql) or die("Quarry Failed");
+
+header("Location: http://localhost/crud/index.php");
+
+mysqli_close($conn);
+
+?>
